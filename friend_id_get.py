87a8cc290b23b0ce0cb25e37468bf76310f687b1
Kazeniwa-Yui <wind.garden0471@gmail.com>
2021-08-25 04:34:41 +0900

    スクリプト大幅修正

--- friend_id_get.py
+++ friend_id_get.py
@@ -17,69 +17,76 @@ api = Twython(
 print('[プログラム開始]')
 time.sleep(1)
 print('探索対象アカウントのスクリーンネームを入力してください')
-sc_name = input('>>> ')
-print('対象アカウント: ' + sc_name)
+sc_name = input('>>> @')
+print('対象アカウント: @' + sc_name)
 print('-'*30)
 time.sleep(1)
 print('[処理開始]FF内アカウントリストの作成を開始します')
 time.sleep(1)
 
-follower_id_list = api.get_followers_ids(screen_name = sc_name, stringify_ids = True)
-follow_id_list = api.get_friends_ids(screen_name=sc_name, stringify_ids = True)
+user_data = api.show_user(screen_name=sc_name)
+follow_count = user_data['friends_count']
+followers_count = user_data['followers_count']
 
-ff_id_list = set(follow_id_list['ids']) & set(follower_id_list['ids'])
+try:
+    if follow_count >= followers_count:
+        f_id = api.get_followers_ids(screen_name=sc_name,stringify_ids=True)
 
-count = 0
-rate = 0
-ff_list = []
+    else:
+        f_id = api.get_friends_ids(screen_name=sc_name,stringify_ids=True)
+    
+    f_id_list = list(f_id['ids'])
+
+    if follow_count >= followers_count:
+        f_list = api.get_friends_list(screen_name=sc_name,count=200)
+
+    else:
+        f_list = api.get_followers_list(screen_name=sc_name,count=200)
+
+    f_user_list = {}
+
+    while True:
+        for one in f_list['users']:
+            id_str = one['id_str']
+            user_name = one['screen_name'] + '/' + one['name']
+            f_user_list[id_str] = user_name
+
+        if f_list['next_cursor'] == 0:
+            break
+
+        cursor = f_list['next_cursor']
+        f_list = api.get_friends_list(screen_name=sc_name,count=200,cursor=cursor)
+
+except twython.exceptions.TwythonError as e:
+    print(e)
+
+ff_id_list = set(f_user_list) & set(f_id_list)
+
 today = datetime.datetime.fromtimestamp(time.time())
 file_name = today.strftime('%Y%m%d%H%M%S') + '.txt'
 f = open(file_name, 'w', encoding='UTF-8')
 f.write('friend_list = [' + '\n')
 
-while True:
-    for one in ff_id_list:
-        try:
-            if rate != 800: #レート制限
-                user_data = api.show_user(user_id=one)
+for one in ff_id_list:
 
-                write_data = '#' + user_data['name'] + ':' + user_data['screen_name'] + '\n"' + one + '",\n'
-            
-                ff_list.append(user_data['name'])
-                count += 1
-                rate += 1
-                f.write(write_data)
+    write_data = '#' + f_user_list[one] + '\n"' + one + '",\n'
+    f.write(write_data)
 
-                print('\r' + str(one), end='')
+    print('\r' + str(one), end='')
 
-            else:
-                print('\n[warning]レート制限に達しました。回復まで15分かかります\n少々お待ちください')
-                for i in range(900, 0, -1):
-                    print('\r残り' + str(i) + '秒', end='')
-                    time.sleep(1)
-                print('\n[制限回復]処理を再開します')
-                rate = 0
-                time.sleep(2)
+f.close()
 
-        except twython.exceptions.TwythonError as e:
-            print(e)
-            break
-    
-    time.sleep(1)
-    print('-'*30)
-    print('\n[処理終了]FF内アカウントリストのファイル書き出し処理を終了しました')
-    time.sleep(1)
-    print('書き出しを実行したアカウントは以下の通りです')
-    time.sleep(1)
-    for one in ff_list:
-        print(one)
-        time.sleep(0.01)
-    time.sleep(1)
-    print('-'*30)
-    print('計' + str(count) + 'アカウントを記録しました')
-    time.sleep(1)
-    f.close()
-    print('出力ファイル名:' + file_name)
-    print('同ディレクトリに正常に出力されました')
-    print('[プログラム終了]')
-    break
+print('\n' + '-'*30)
+print('\n[処理終了]FF内アカウントリストのファイル書き出し処理を終了しました')
+time.sleep(1)
+print('書き出しを実行したアカウントは以下の通りです')
+time.sleep(1)
+for one in ff_id_list:
+    print(f_user_list[one])
+    time.sleep(0.05)
+print('-'*30)
+print('計' + str(len(ff_id_list)) + 'アカウントを記録しました')
+time.sleep(1)
+print('出力ファイル名:' + file_name)
+print('同ディレクトリに正常に出力されました')
+print('[プログラム終了]')
